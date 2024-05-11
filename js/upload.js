0e3a904b0c000bf156fe4f6297fea4bd5fa4c81c
H4CK3R-5M4CK3R <=>
2024-05-12 04:21:45 +0530

    "Update CSS and JS for file upload progress, add captcha and link sharing functionality"
    This commit updates the CSS for the file upload progress bar, increasing its height and adding margin for better visibility. It also modifies the animation for the

--- js/upload.js
+++ js/upload.js
@@ -1,25 +1,129 @@
-//DOM
 const $ = document.querySelector.bind(document);
 
-//APP
+function setProgressBarPercentage(percentage) {
+  const backgroundSize = `${percentage}% 100%`;
+  const progressBars = document.querySelectorAll('.upload .upload-files footer .list-files .file .progress');
+  progressBars.forEach(progressBar => {
+    progressBar.style.backgroundSize = backgroundSize;
+  });
+}
+
+function startCaptcha() {
+  return new Promise(function(resolve, reject) {
+      grecaptcha.ready(function () {
+          grecaptcha.execute('6LevqiclAAAAACiWAicy45fF4soqeKs9mCyfIWY2', { action: 'download' })
+              .then(function (token) {
+                  resolve(token); // Resolve with the captcha token
+              }).catch(function (error) {
+                  reject(error); // Reject with error if captcha execution fails
+              });
+      });
+  });
+}
+
+function shareLink() {
+  const link = document.querySelector('.lnk').getAttribute('href');
+  if (navigator.share) {
+      navigator.share({
+          title: 'Download Link',
+          text: 'Check out this file: ',
+          url: link,
+      })
+      .then(() => console.log('Shared successfully'))
+      .catch((error) => console.error('Error sharing:', error));
+  } else {
+      console.log('Web Share API not supported.');
+  }
+}
+
+function copyLink(event) {
+  event.preventDefault();
+  const link = document.querySelector('.lnk').getAttribute('href');
+  navigator.clipboard.writeText(link).then(() => {
+    console.log('Link copied to clipboard:', link);
+  }).catch((error) => {
+    console.error('Failed to copy link:', error);
+  });
+}
+
+function uploadFile(file, captchaToken) {
+  const formData = new FormData();
+  formData.append('file', file);
+  formData.append('token', 'pryanshutest');
+  const xhr = new XMLHttpRequest();
+  xhr.upload.onprogress = function(event) {
+    const percentComplete = (event.loaded / event.total) * 100;
+    setProgressBarPercentage(parseInt(percentComplete.toFixed(2), 10));
+  };
+  xhr.onload = function() {
+    if (xhr.status === 200) {
+      const response = JSON.parse(xhr.responseText);
+      if (response.status) {
+        document.getElementsByClassName("done-upload")[0].style.display = "block";
+        document.getElementsByClassName("copy-upload")[0].style.display = "block";
+        document.getElementsByClassName("lnk")[0].href = response.link
+        if (navigator.share) {
+          document.getElementsByClassName("share-upload")[0].style.display = "block";
+        }
+      } else {
+        document.getElementsByClassName("error-upload")[0].style.display = "block";
+      }
+    } else {
+      document.getElementsByClassName("error-upload")[0].style.display = "block";
+    }
+  };
+  xhr.onerror = function() {
+    document.getElementsByClassName("error-upload")[0].style.display = "block";
+  };
+  xhr.open('POST', 'https://download.zerotwo.in/upload');
+  xhr.send(formData);
+}
+
 let App = {};
 App.init = function () {
-  //Init
   function handleFileSelect(evt) {
-    const files = evt.target.files; // FileList object
-
-    //files template
+    const files = evt.target.files;
     let template = `${Object.keys(files).
       map(file => `<div class="file file--${file}">
      <div class="name"><span>${files[file].name}</span></div>
      <div class="progress active"></div>
-     <div class="done">
-	    <a href="" target="_blank">
+     <div class="done done-upload" style="display: none; margin-left: 1%;">
+	    <a href="" target="_blank" class="lnk" title="Open Link">
           <svg xmlns="https://www.w3.org/2000/svg" xmlns:xlink="https://www.w3.org/1999/xlink" version="1.1" x="0px" y="0px" viewBox="0 0 1000 1000">
         <g><path id="path" d="M500,10C229.4,10,10,229.4,10,500c0,270.6,219.4,490,490,490c270.6,0,490-219.4,490-490C990,229.4,770.6,10,500,10z M500,967.7C241.7,967.7,32.3,758.3,32.3,500C32.3,241.7,241.7,32.3,500,32.3c258.3,0,467.7,209.4,467.7,467.7C967.7,758.3,758.3,967.7,500,967.7z M748.4,325L448,623.1L301.6,477.9c-4.4-4.3-11.4-4.3-15.8,0c-4.4,4.3-4.4,11.3,0,15.6l151.2,150c0.5,1.3,1.4,2.6,2.5,3.7c4.4,4.3,11.4,4.3,15.8,0l308.9-306.5c4.4-4.3,4.4-11.3,0-15.6C759.8,320.7,752.7,320.7,748.4,325z"</g>
         </svg>
 			</a>
      </div>
+
+     <div class="copy-upload" style="display: none;">
+        <a href="" target="_blank" onclick="copyLink(event)" title="Copy">
+            <?xml version="1.0" ?><svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" width="20px" height="20px"><path d="M16,20H8a3,3,0,0,1-3-3V7A1,1,0,0,0,3,7V17a5,5,0,0,0,5,5h8a1,1,0,0,0,0-2Zm-6-7a1,1,0,0,0,1,1h5a1,1,0,0,0,0-2H11A1,1,0,0,0,10,13ZM21,8.94a1.31,1.31,0,0,0-.06-.27l0-.09a1.07,1.07,0,0,0-.19-.28h0l-6-6h0a1.07,1.07,0,0,0-.28-.19.32.32,0,0,0-.09,0A.88.88,0,0,0,14.05,2H10A3,3,0,0,0,7,5V15a3,3,0,0,0,3,3h8a3,3,0,0,0,3-3V9S21,9,21,8.94ZM15,5.41,17.59,8H16a1,1,0,0,1-1-1ZM19,15a1,1,0,0,1-1,1H10a1,1,0,0,1-1-1V5a1,1,0,0,1,1-1h3V7a3,3,0,0,0,.18,1H11a1,1,0,0,0,0,2h8Z" fill="#6563ff"/></svg>
+        </a>
+    </div>
+
+    <div class="short-upload" style="display: none;">
+      <a href="" target="_blank" title="Short Link">
+          <?xml version="1.0" ?><svg height="20px" width="20px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" xmlns:cc="http://creativecommons.org/ns#" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"><g transform="translate(0 -1028.4)"><g transform="matrix(1.0607 1.0607 -1.0607 1.0607 1146.8 34.926)"><path d="m-63 1003.4v11.3 0.7 1l2 2 2-2v-1-0.7-11.3h-4z" fill="#ecf0f1"/><path d="m-61 1003.4v15l2-2v-1-0.7-11.3h-2z" fill="#bdc3c7"/><rect fill="#e67e22" height="11" width="4" x="-63" y="1004.4"/><path d="m-61 1000.4c-1.105 0-2 0.9-2 2v1h4v-1c0-1.1-0.895-2-2-2z" fill="#7f8c8d"/><g transform="translate(-7,1)"><path d="m-55.406 1016 1.406 1.4 1.406-1.4h-1.406-1.406z" fill="#34495e"/><path d="m-54 1016v1.4l1.406-1.4h-1.406z" fill="#2c3e50"/></g><path d="m-61 1000.4c-1.105 0-2 0.9-2 2v1h2v-3z" fill="#95a5a6"/><rect fill="#d35400" height="11" width="2" x="-61" y="1004.4"/></g></g></svg>
+      </a>
+      <?xml version="1.0" ?><svg height="20px" version="1.1" width="20px" xmlns="http://www.w3.org/2000/svg" xmlns:cc="http://creativecommons.org/ns#" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"><g transform="translate(0 -1028.4)"><g transform="matrix(1.0607 1.0607 -1.0607 1.0607 1146.8 34.926)"><path d="m-63 1003.4v11.3 0.7 1l2 2 2-2v-1-0.7-11.3h-4z" fill="#ecf0f1"/><path d="m-61 1003.4v15l2-2v-1-0.7-11.3h-2z" fill="#bdc3c7"/><rect fill="#e67e22" height="11" width="4" x="-63" y="1004.4"/><path d="m-61 1000.4c-1.105 0-2 0.9-2 2v1h4v-1c0-1.1-0.895-2-2-2z" fill="#7f8c8d"/><g transform="translate(-7,1)"><path d="m-55.406 1016 1.406 1.4 1.406-1.4h-1.406-1.406z" fill="#34495e"/><path d="m-54 1016v1.4l1.406-1.4h-1.406z" fill="#2c3e50"/></g><path d="m-61 1000.4c-1.105 0-2 0.9-2 2v1h2v-3z" fill="#95a5a6"/><rect fill="#d35400" height="11" width="2" x="-61" y="1004.4"/></g></g></svg>
+    </div>
+
+    <div class="share-upload" style="display: none;">
+        <a href="" target="_blank" onclick="shareLink()" title="Share Link">
+            <?xml version="1.0" ?>
+            <svg height="20px" viewBox="0 0 512 512" width="20px" xmlns="http://www.w3.org/2000/svg">
+                <title/>
+                <path d="M384,336a63.78,63.78,0,0,0-46.12,19.7l-148-83.27a63.85,63.85,0,0,0,0-32.86l148-83.27a63.8,63.8,0,1,0-15.73-27.87l-148,83.27a64,64,0,1,0,0,88.6l148,83.27A64,64,0,1,0,384,336Z"/>
+            </svg>
+        </a>
+    </div>
+
+    <div class="error-upload" style="display: none;">
+        <a href="" target="_blank" title="Error">
+            <?xml version="1.0" ?><!DOCTYPE svg  PUBLIC '-//W3C//DTD SVG 1.0//EN'  'http://www.w3.org/TR/2001/REC-SVG-20010904/DTD/svg10.dtd'><svg height="20px" style="overflow:visible;enable-background:new 0 0 32 32" viewBox="0 0 32 32" width="20" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><g><g id="Error_1_"><g id="Error"><circle cx="16" cy="16" id="BG" r="16" style="fill:#D72828;"/><path d="M14.5,25h3v-3h-3V25z M14.5,6v13h3V6H14.5z" id="Exclamatory_x5F_Sign" style="fill:#E6E6E6;"/></g></g></g></svg>
+        </a>
+    </div>
+
     </div>`).
       join("")}`;
 
@@ -31,10 +135,18 @@ App.init = function () {
     }, 1000);
 
     Object.keys(files).forEach(file => {
-      let load = 2000 + file * 2000; // fake load
+      let load = 1000; // fake load
       setTimeout(() => {
-        $(`.file--${file}`).querySelector(".progress").classList.remove("active");
-        $(`.file--${file}`).querySelector(".done").classList.add("anim");
+        function startCaptchaAndUpload(fil, file) {
+            startCaptcha().then(function(token) {
+                uploadFile(fil, token); // Call uploadFile with the captcha token
+                $(`.file--${file}`).querySelector(".progress").classList.remove("active");
+                $(`.file--${file}`).querySelector(".done").classList.add("anim");
+            }).catch(function(error) {
+                console.error("Error starting captcha:", error);
+            });
+        }
+        startCaptchaAndUpload(files[file], file)
       }, load);
     });
   }
